fd393d587a369c93c849d1b16c1d918240ec46d3
Jaclyn Hardy <jackihardy@yahoo.com>
2020-06-29 21:58:44 -0700

    owner and pet models

---
+++ controllers/petController.js
@@ -0,0 +1,13 @@
+const db = require ("../models")
+
+module.exports = {
+    findAll: function(req, res) {
+        db.User
+         .find({
+             sitte
+         })
+    }
+}
+
+
+
---
+++ models/owner.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ownerProfile = new Schema ({
+    firstName:{type: String, required: true},
+    lastName:{type: String, required: true},
+    userZipcode:{type: Number, required: true},
+    hasPet: {type: Boolean, required: true},
+
+    Pet : [{ // One pet can have many photo
+        type: Schema.Types.ObjectId, //foreign key from Pet model
+        ref: "Pet" //Need to check the Pet model later
+    }]
+
+})
+
+let Owner = mongoose.model("Owner", userProfile);
+
+module.exports = Owner; 
