389acd3751073d5bc8b0c0bcd7c1674e825f55c9
Anna Vu <ngavu2712@gmail.com>
2020-07-02 22:55:33 -0700

    create queries to create a pet profile, get pet profile and remove pet profile

--- controllers/petController.js
+++ controllers/petController.js
@@ -1,3 +1,33 @@
+const db = require ("../models")
+
+module.exports = {
+    
+    // Pet Input
+    create : function(req, res) {
+        console.log(req.body)
+        db.Pet
+        .create(req.body)
+        .then(dbOwner => res.json(dbOwner))
+    },
+
+    // Get all user's pet info
+    findById: function(req,res) {
+        db.Pet
+        .findById(req.params.id)
+        .then(dbOwner => res.json(dbOwner))
+        .catch(err => status(422).json(err))
+    },
+
+    // Remove a pet
+    remove: function (req,res){
+        db.Pet
+        .findById( {_id: req.params.id} )
+        .then( deletedPet => deletedPet.remove() )
+        .then(deletedPet => res.json(deletedPet))
+        .catch(err => res.status(422).json(err))
+    }
+
+}
 
 
 
