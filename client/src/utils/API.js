0c27a3d25e6dd0b49e87e8ad62e9785a707a86bd
Anna Vu <ngavu2712@gmail.com>
2020-07-02 23:10:33 -0700

    adding API routes for pet

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,9 +7,26 @@ export default {
         return axios.post("/api/ownerprofile", userInfo).then(result => result.data);
     },
 
+    //User login
     LogIn: function(id) {
         return axios.get("/api/ownerprofile" + id)
         .then(result => result.data);
+    },
+
+    // User add Pet
+    addPet: function(petInfo) {
+        return axios.post("/api/petprofile", petInfo).then(result => result.data);
+    },
+
+    // Retrieve Pet info and render it on user profile page
+    getAllPet : function() {
+        return axios.get("/api/petprofile").then(result => result.data);
+    },
+
+    // Delete a Pet
+    deleteAPet: function(id) {
+        return axios.delete("/api/petprofile" + id).then(result => result.data);
+
     }
 
 
