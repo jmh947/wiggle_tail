d09f1cf424cf6b25bcd3b4febe57a57dfa1b6665
Anna Vu <ngavu2712@gmail.com>
2020-07-06 21:31:50 -0700

    adding API endpoint for petfinder API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 
+let accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI4U2NJd0ZpaEdGdmYzamFxNFUzc1NEaUpVV1pGb25Jd3RhNFNsbDJ3TDVwMnRXaHlIZiIsImp0aSI6ImEwYWIxZDg1YWYwYTBjYjIyMWZiNjM5ZDQwNjI4ZGI4NzViOTNhOGEzOTMxNTFhYmE1M2UzYzBlZTY3YzlhNTkwM2M3NTUxYmI1OWM0NGIyIiwiaWF0IjoxNTk0MDkyOTA3LCJuYmYiOjE1OTQwOTI5MDcsImV4cCI6MTU5NDA5NjUwNywic3ViIjoiIiwic2NvcGVzIjpbXX0.id2___h_I5PEWXar_xFUtepkfeOZfzyEDgrjURHD6E0Xq5-N70sl08SgHd0IZfh2rPCZaTiEOIUCUDDrFySZOYMZYumHCYBUEgs1pMK3HevKQKtnPPQOXXhSpgc7Tv7Qk_6-z2dGYxHaFl6y4BPdlf-pG5LYg1KCEhvKLEFsgYonbe1cRA6kCgVOXUdPI895dQwIXDRRJWv4XgF2s4o0oVT8PUaDltFMpxwm6UMTwYXIRHhJllJsx3bKA1L8hPmaN9PweSA83QKn3mR_Srzt3yZr5CMs9kW6o33ToCy3w0OtkJ7xnG8mQH4-sM2bS-Y4O0dw7DntRxZC1rqlLLMC_Q"
+
 export default {
+
+    //API query to pet finder
+    getAnimals : function(query) {
+        return axios.get("https://api.petfinder.com/v2/animals?")
+    },
 
     // User's input information
     addOwnerInfo : function(userInfo) {
