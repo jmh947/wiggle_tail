a58a97ec31f639379ae86356d947077ef41a693c
Christina Tarango <liwentarango@gmail.com>
2020-07-10 19:15:05 -0700

    petfinder is done and mongo setup

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import PostDetails from './components/posts/PostDetails';
 import SignUp from './components/auth/Signup';
 import SignIn from './components/auth/Signin';
 import CreatePost from './components/posts/CreatePost';
-import petFinder from './components/petFinder/petFinder0';
+
 import petFinder1 from './components/petFinder/petFinder';
 
 
@@ -21,7 +21,6 @@ function App() {
    <Route path='/signin' component={SignIn} />
    <Route path='/signup' component={SignUp} />
    <Route path='/createpost' component={CreatePost} />
-   <Route path='/petfinder' component={petFinder} />
    <Route path='/petfinder1' component={petFinder1} />
  </Switch>
   
--- client/src/components/layout/SignedInLinks.js
+++ client/src/components/layout/SignedInLinks.js
@@ -7,8 +7,7 @@ const SignedInLinks = (props) => {
     return (
        <ul className="right">
            <li><NavLink to="/createpost">Posting Board</NavLink></li>
-           <li><NavLink to="/petfinder">Pet Finder</NavLink></li>
-           <li><NavLink to="/petfinder1">Pet Finder1</NavLink></li>
+           <li><NavLink to="/petfinder1">Pet Finder</NavLink></li>
            <li><a onClick={props.signOut}>Log Out</a></li>
     <li><NavLink to="/" className="btn btn-floating pink lighten-1">{props.profile.initials}</NavLink></li>
        </ul>
